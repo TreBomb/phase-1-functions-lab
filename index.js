c295e53986f4051d004f1c912cb08967d203fa44
Trestin <trestinishak@gmail.com>
2021-08-09 21:53:01 -0500

    commit tree 3e7f79fd4229de80eafb4bfc4c545d1851adaf42

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks(distance) {
+    return(Math.abs(42-distance));
+}
+
+function distanceFromHqInFeet(distance) {
+    return(distanceFromHqInBlocks(distance) * 264);
+}
+
+function distanceTravelledInFeet(street1, street2) {
+    return(Math.abs(distanceFromHqInFeet(street1) - distanceFromHqInFeet(street2)));
+}
+
+function calculatesFarePrice(street1, street2) {
+    let distance = distanceTravelledInFeet(street1, street2);
+    if(distance <= 400) {
+        return(0);
+    } else if(distance >= 400 && distance <= 2000) {
+        return((distance - 400) * 0.02);
+    } else if(distance >= 2000 && distance <= 2500) {
+        return(25);
+    } else if(distance >= 2500) {
+        return(`cannot travel that far`);
+    }
+}
